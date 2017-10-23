a432e90a7192884eba9344753e3b2bd46083ed09
Niklas Gollenstede <n.gollenstede@web.de>
2017-10-23 17:29:16 +0200

    rename {install.js ==> installer.js}

--- start.js
+++ start.js
@@ -8,7 +8,7 @@ switch (args[0]) {
 	case undefined: // started from file system / without args ==> install
 	case 'install': case 'refresh': case 'uninstall': {
 		const command = args[0] || 'install';
-		(await require('./install.js')[command]({
+		(await require('./installer.js')[command]({
 			source: process.argv[1].startsWith(require('path').resolve('/snapshot/')) ? process.argv[0] : __dirname,
 		}));
 		dialog('info', 'Native-Ext', `Operation ${ command } successful`);
